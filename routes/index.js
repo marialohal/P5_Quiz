49e8bdeb184d4ae9ab2c2d951b282283fb019e2f
María <36447944+marialohal@users.noreply.github.com>
2018-05-04 13:01:00 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const Sequelize = require("sequelize");
-const {models} = require ('../models/index');
+
 
 /* GET home page. */
 router.get('/',function(req,res,next){
-    res.render('index',{title:'Quiz'});
+    res.render('index',{title:'Express'});
 });
 
 router.get('/credits', function(req, res, next) {
@@ -13,14 +12,7 @@ router.get('/credits', function(req, res, next) {
 });
 
 router.get('/quizzes',function(req, res,next){
-    models.quiz.findAll()
-        .then(quizzes =>{
-            var JSONQuizzes =JSON.stringify(quizzes);
-            res.render('quizzes', {quizzes: JSONQuizzes});
-    })
-    .catch(error => {
-        console.log(error);
-    })
+    res.render('index');
 });
 
 module.exports = router;
